1578dc93b6f8068401c4fa8967df0aa0a772d823
Julia Ann Jose <juliaannjose@gmail.com>
2023-01-02 12:26:46 +0300

    fix: add python package requirements

--- setup.py
+++ setup.py
@@ -2,5 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="milvus_search_system",
+    version="0.1.0",
     packages=find_packages(),
+    install_requires=[
+        "pyspark==3.3.1",
+        "pymilvus==2.2.0",
+        "sentence_transformers==2.2.2",
+    ],
 )
